ff9fc3eeb2b2900f9932af1632f45444bb98f503
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-09-29 21:34:31 -0400

    Add components

---
+++ src/components/AddItemInput.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div``;
+
+export default function AddItemInput({ addItem }) {
+  const [itemTitle, setItemTitle] = React.useState('');
+
+  const handleAdd = () => {
+    const successful = addItem(itemTitle);
+    if (successful) {
+      setItemTitle('');
+    } else {
+      // handle add error
+    }
+  };
+
+  const handleKeyPress = e => {
+    const { key } = e;
+    if (key === 'Enter') handleAdd();
+  };
+
+  return (
+    <Styles>
+      <input
+        type="text"
+        placeholder="New item"
+        value={itemTitle}
+        onChange={e => setItemTitle(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={handleAdd}>Add</button>
+    </Styles>
+  );
+}
---
+++ src/components/AddListInput.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div``;
+
+export default function AddListInput({ addList }) {
+  const [listTitle, setListTitle] = React.useState('');
+
+  const handleAdd = () => {
+    const successful = addList(listTitle);
+    if (successful) {
+      setListTitle('');
+    } else {
+      // handle add error
+    }
+  };
+
+  const handleKeyPress = e => {
+    const { key } = e;
+    if (key === 'Enter') handleAdd();
+  };
+
+  return (
+    <Styles>
+      <input
+        type="text"
+        placeholder="New list"
+        value={listTitle}
+        onChange={e => setListTitle(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={handleAdd}>Add</button>
+    </Styles>
+  );
+}
