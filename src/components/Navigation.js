8558e47788a086c36553739cc5f99702c2aaa9e4
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-01 16:10:46 -0400

    Add basic nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from 'contexts/AuthContext';
 import styled from 'styled-components';
 
 const Styles = styled.div``;
 
 export default function Navigation() {
+  const { user } = useAuth();
+
   return (
     <Styles>
-      <h2>Navigation</h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {user ? (
+          <li>
+            {user.email} - <Link to="/logout">Log out</Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="/login">Log in</Link>
+          </li>
+        )}
+        <li>
+          <Link to="/lists">My lists</Link>
+        </li>
+      </ul>
     </Styles>
   );
 }
