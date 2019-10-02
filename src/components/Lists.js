fdd948644d113f1ecf90272f83a458fed87e4420
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-01 23:35:00 -0400

    Extract Firestore logic

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,45 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import useLocalStorageState from 'hooks/useLocalStorageState';
 import { Link } from 'react-router-dom';
 import AddListInput from './AddListInput';
-import uuid from 'uuid/v4';
-import slugify from 'url-slug';
+import useLists from 'hooks/useLists';
 
 const Styles = styled.div``;
 
-const constructNewList = title => ({
-  id: uuid(),
-  title,
-  slug: slugify(title)
-});
-
 export default function Lists() {
-  const [listsData, setListsData] = useLocalStorageState('lists-data', [
-    { id: '1', title: 'Groceries', slug: 'groceries' },
-    { id: '2', title: 'Books to read', slug: 'books-to-read' }
-  ]);
-
-  React.useEffect(() => {
-    // fetch lists
-  }, []);
-
-  const createNewList = title => {
-    setListsData(prev => [...prev, constructNewList(title)]);
-  };
-
-  const removeList = id => {
-    setListsData(prev => prev.filter(l => l.id !== id));
-  };
+  const [data, actions] = useLists();
 
   return (
     <Styles>
       <h2>My Lists</h2>
-      <AddListInput addList={createNewList} />
-      {listsData.map(l => (
+      <AddListInput addList={actions.createList} />
+      {data.length === 0 && <h3>No lists</h3>}
+      {data.map(l => (
         <div key={l.id}>
           <Link to={`/lists/${l.slug}`}>{l.title}</Link>{' '}
-          <button onClick={() => removeList(l.id)}>X</button>
+          <button onClick={() => actions.removeList(l.id)}>X</button>
+          <button
+            onClick={() => {
+              const title = window.prompt('New title');
+              actions.retitleList(l.id, title);
+            }}
+          >
+            Edit
+          </button>
         </div>
       ))}
     </Styles>
