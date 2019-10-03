99008e25f044297456338d251f11cac02ffac61e
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-02 21:12:06 -0400

    Clean up actions

--- src/hooks/useItems.js
+++ src/hooks/useItems.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useLocalStorageState from './useLocalStorageState';
 import firestore from 'fb/firestore';
 import { fieldValues, collections } from 'fb/config';
@@ -15,36 +15,44 @@ const constructNewItem = (title, userId, listId) => ({
   updatedAt: fieldValues.serverTimestamp()
 });
 
-const addUpdateTimestamp = item => ({
-  ...item,
-  updatedAt: fieldValues.serverTimestamp()
-});
-
 export default function useItems(listSlug) {
   const { user } = useAuth();
   const [list, setList] = useLocalStorageState(`list-${listSlug}`, null);
   const [items, setItems] = useLocalStorageState(`items-${listSlug}`, []);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    if (user) {
-      const unsubscribe = firestore
-        .collection(collections.LISTS)
-        .where('slug', '==', listSlug)
-        .where('userId', '==', user.uid)
-        .limit(1)
-        .onSnapshot(snapshot => {
-          const records = snapshot.docs.map(doc => ({
-            id: doc.id,
-            ...doc.data()
-          }));
-          // setListsData(records);
-          console.log(records);
-          setList(records[0]);
-        });
-      return () => unsubscribe();
+  const wrapRequest = (callback, errorMessage) => async (...args) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      await callback(...args);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setError(errorMessage);
+      setIsLoading(false);
     }
+  };
+
+  // fetch list
+  useEffect(() => {
+    const unsubscribe = firestore
+      .collection(collections.LISTS)
+      .where('slug', '==', listSlug)
+      .where('userId', '==', user.uid)
+      .limit(1)
+      .onSnapshot(snapshot => {
+        const records = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        setList(records[0]);
+      });
+    return () => unsubscribe();
   }, [listSlug, setList, user]);
 
+  // fetch list's items
   useEffect(() => {
     if (user && list) {
       const unsubscribe = firestore
@@ -63,71 +71,48 @@ export default function useItems(listSlug) {
     }
   }, [list, setItems, user]);
 
-  const retitleList = async title => {
-    try {
-      await firestore
-        .collection(collections.LISTS)
-        .doc(list.id)
-        .update(
-          addUpdateTimestamp({
-            title
-          })
-        );
-    } catch (error) {
-      console.error('Error updating document: ', error);
-    }
-  };
+  const retitleList = wrapRequest(async title => {
+    await firestore
+      .collection(collections.LISTS)
+      .doc(list.id)
+      .update({
+        title,
+        updatedAt: fieldValues.serverTimestamp()
+      });
+  }, 'Cannot change list title');
 
-  const createItem = async title => {
-    try {
-      await firestore
-        .collection(collections.ITEMS)
-        .add(constructNewItem(title, user.uid, list.id));
-    } catch (error) {
-      console.error('Error adding document: ', error);
-    }
-  };
+  const createItem = wrapRequest(async title => {
+    await firestore
+      .collection(collections.ITEMS)
+      .add(constructNewItem(title, user.uid, list.id));
+  }, 'Cannot create new item');
 
-  const removeItem = async id => {
-    try {
-      await firestore
-        .collection(collections.ITEMS)
-        .doc(id)
-        .delete();
-    } catch (error) {
-      console.error('Error removing document: ', error);
-    }
-  };
+  const removeItem = wrapRequest(async id => {
+    await firestore
+      .collection(collections.ITEMS)
+      .doc(id)
+      .delete();
+  }, 'Cannot remove item');
 
-  const toggleComplete = async (id, isComplete) => {
-    try {
-      await firestore
-        .collection(collections.ITEMS)
-        .doc(id)
-        .update(
-          addUpdateTimestamp({
-            isComplete: !isComplete
-          })
-        );
-    } catch (error) {
-      console.error('Error updating document: ', error);
-    }
-  };
+  const toggleComplete = wrapRequest(async (id, isComplete) => {
+    await firestore
+      .collection(collections.ITEMS)
+      .doc(id)
+      .update({
+        isComplete: !isComplete,
+        updatedAt: fieldValues.serverTimestamp()
+      });
+  }, 'Cannot toggle item complete');
 
-  const toggleShowComplete = async () => {
-    try {
-      await firestore
-        .collection(collections.LISTS)
-        .doc(list.id)
-        .update(
-          addUpdateTimestamp({
-            showComplete: !list.showComplete
-          })
-        );
-    } catch (error) {
-      console.error('Error updating document: ', error);
-    }
-  };
+  const toggleShowComplete = wrapRequest(async () => {
+    await firestore
+      .collection(collections.LISTS)
+      .doc(list.id)
+      .update({
+        showComplete: !list.showComplete,
+        updatedAt: fieldValues.serverTimestamp()
+      });
+  }, 'Cannot toggle show/hide complete items');
 
   const actions = {
     retitleList,
@@ -139,7 +124,9 @@ export default function useItems(listSlug) {
 
   const data = {
     list,
-    items
+    items,
+    isLoading,
+    error
   };
 
   return [data, actions];
