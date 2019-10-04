1222e65a6eab315a9b597a609d8e25c05dc2f91b
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-04 13:12:32 -0400

    Add styles

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 5000;
+  display: flex;
+  justify-content: center;
+  padding-top: 15%;
+  background: white;
+`;
 
 export default function Loading() {
-  return <div>Loading...</div>;
+  return <Styles>Loading...</Styles>;
 }
