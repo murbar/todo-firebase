7f565f42f0bc262269888439c3c9a9d9ebd1d692
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-04 13:14:00 -0400

    Add form components

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -2,9 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { Redirect, useLocation } from 'react-router-dom';
 import Button from 'components/common/Button';
+import Input from 'components/common/Input';
 import useForm from 'hooks/useForm';
 import { useAuth } from 'contexts/AuthContext';
 import config from 'config';
+import FormField from 'components/common/FormField';
 
 const Styles = styled.div``;
 
@@ -24,30 +26,38 @@ export default function Login() {
     <Styles>
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
-        <input
-          name="email"
-          type="text"
-          placeholder="Username"
-          onChange={handleChange}
-          value={values.email || ''}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          onChange={handleChange}
-          value={values.password || ''}
-        />
-        <label htmlFor="">
-          Remember me?
-          <input
-            name="remember"
-            type="checkbox"
-            checked={values.remember === undefined ? true : values.remember}
+        <FormField>
+          <Input
+            name="email"
+            type="text"
+            placeholder="Username"
             onChange={handleChange}
+            value={values.email || ''}
           />
-        </label>
-        <Button type="submit">Sign up</Button>
+        </FormField>
+        <FormField>
+          <Input
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={handleChange}
+            value={values.password || ''}
+          />
+        </FormField>
+        <FormField>
+          <label htmlFor="">
+            Remember me?
+            <input
+              name="remember"
+              type="checkbox"
+              checked={values.remember === undefined ? true : values.remember}
+              onChange={handleChange}
+            />
+          </label>
+        </FormField>
+        <FormField>
+          <Button type="submit">Sign up</Button>
+        </FormField>
       </form>
     </Styles>
   );
