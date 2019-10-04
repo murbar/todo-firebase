b982f2b87ac23d8e987365f60a541d05423a2516
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-04 13:12:18 -0400

    Add styles

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Styles = styled.div``;
+const Styles = styled.div`
+  background: white;
+  margin: 0 0 1rem 0;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  display: flex;
+`;
 
-export default function ListItem({ data, actions }) {
+const Toggle = styled.div`
+  width: 4rem;
+  text-align: center;
+`;
+
+const Title = styled.div`
+  flex: 1;
+`;
+
+const Controls = styled.div`
+  width: 10rem;
+`;
+
+export default function ListItem({ data, actions, index }) {
   const { id, title, isComplete } = data;
 
-  const toggle = () => actions.toggleComplete(id, isComplete);
+  const toggle = () => actions.toggleItemComplete(id, isComplete);
 
   const remove = () => actions.removeItem(id);
 
   return (
     <Styles>
-      <input type="checkbox" checked={isComplete} onChange={toggle} /> {title}{' '}
-      <button onClick={remove}>X</button>
+      <Toggle>
+        <input type="checkbox" checked={isComplete} onChange={toggle} />
+      </Toggle>
+
+      <Title>{title}</Title>
+
+      <Controls>
+        <button onClick={remove}>X</button>
+        {index !== 0 && (
+          <button onClick={() => actions.reorderItems(index, index - 1)}>{'<<'}</button>
+        )}
+        <button onClick={() => actions.reorderItems(index, index + 1)}>{'>>'}</button>
+      </Controls>
     </Styles>
   );
 }
