843ca92247572a272c775c79fb75ebe21bbb1247
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-06 13:01:57 -0400

    Add badge

--- src/components/List.js
+++ src/components/List.js
@@ -4,6 +4,21 @@ import { useParams, useHistory, Link } from 'react-router-dom';
 import ListItem from './ListItem';
 import AddItemInput from './AddItemInput';
 import useListItems from 'hooks/useListItems';
+import MainHeading from './common/MainHeading';
+
+const Badge = styled.span`
+  display: inline-flex;
+  color: ${p => p.theme.colors.background};
+  background: ${p => (p.label ? p.theme.colors[p.label] : 'blue')};
+  margin-left: 0.5em;
+  width: 1.25em;
+  height: 1.25em;
+  border-radius: 50%;
+  font-size: 0.6em;
+  justify-content: center;
+  align-items: center;
+  line-height: 1;
+`;
 
 const Styles = styled.div``;
 
@@ -11,29 +26,31 @@ export default function List() {
   const { slug } = useParams();
   const history = useHistory();
   const [itemsData, itemsActions] = useListItems(slug);
+  const { list, items } = itemsData;
 
   if (itemsData.noSuchList) history.replace('/lists');
 
-  const uncompletedItemCount = itemsData.items.reduce(
+  const uncompletedItemCount = items.reduce(
     (count, i) => (!i.isComplete ? count + 1 : count),
     0
   );
   const itemsDisplay =
-    itemsData.list && itemsData.list.showComplete
-      ? itemsData.items
-      : itemsData.items.filter(i => !i.isComplete);
+    list && list.showComplete ? items : items.filter(i => !i.isComplete);
 
   return (
     <Styles>
-      {itemsData.list ? (
+      {list ? (
         <>
-          <h2>
-            {itemsData.list.title} ({uncompletedItemCount})
-          </h2>
+          <MainHeading label={list.label}>
+            {list.title}
+            {uncompletedItemCount > 0 && (
+              <Badge label={list.label}>{uncompletedItemCount}</Badge>
+            )}
+          </MainHeading>
           <Link to="/lists">← Lists</Link>
           <AddItemInput addItem={itemsActions.createItem} />
           <button onClick={itemsActions.toggleShowComplete}>
-            {itemsData.list.showComplete ? 'Hide' : 'Show'} completed
+            {list.showComplete ? 'Hide' : 'Show'} completed
           </button>
           {itemsDisplay.length ? (
             <div>
