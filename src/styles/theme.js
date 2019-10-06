f1f2b88a3a2bbabc799649c1e9d7cb3df80b839c
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-10-06 13:00:31 -0400

    Add accent colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,21 +1,56 @@
 import { media } from './helpers';
 
+const accentsLight = {
+  blue: '#027afe',
+  purple: '#bf78d9',
+  red: '#fd3e30',
+  orange: '#fd9500',
+  yellow: '#fccd01',
+  green: '#13c758'
+};
+
+const accentsDark = {
+  blue: '#0c84fe',
+  purple: '#d47ff5',
+  red: '#fd463a',
+  orange: '#fea00a',
+  yellow: '#fed60a',
+  green: '#31d15b'
+};
+
 const colors = {
-  offWhite: '#efefef',
-  offBlack: '#222',
-  green: '#3cc473'
+  white: '#FFFFFF',
+  black: '#000000',
+  offWhite: '#f4f4f4',
+  offBlack: '#393939'
 };
 
 const theme = {
-  colors: {
-    ...colors,
-    background: colors.offWhite,
-    foreground: colors.offBlack,
-    primary: colors.green
-  },
   inputBorderRadius: `0.5rem`,
   fontFamily: "'Source Sans Pro', sans-serif",
   media
 };
 
-export default theme;
+const light = {
+  colors: {
+    background: colors.white,
+    foreground: colors.offBlack,
+    primary: colors.blue,
+    ...colors,
+    ...accentsLight
+  },
+  ...theme
+};
+
+const dark = {
+  colors: {
+    background: colors.black,
+    foreground: colors.offWhite,
+    primary: colors.blue,
+    ...colors,
+    accentsDark
+  },
+  ...theme
+};
+
+export default { light, dark };
