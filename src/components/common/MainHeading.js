dd354d81b6d2f86fac6c737be26aff9f6c85f8e4
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-11-15 11:32:43 -0500

    Vertically center contents

--- src/components/common/MainHeading.js
+++ src/components/common/MainHeading.js
@@ -3,4 +3,6 @@ import styled from 'styled-components';
 export default styled.h1`
   color: ${p => (p.label ? p.theme.colors[p.label] : 'inherit')};
   font-size: 3.5rem;
+  display: flex;
+  align-items: center;
 `;
