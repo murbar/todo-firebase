447b1fd12df3f551798a84aebca3fe87554a44f2
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-11-16 15:52:40 -0500

    Handle drag end

--- src/components/DraggableReorderList.js
+++ src/components/DraggableReorderList.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import ListItem from './ListItem';
+import styled from 'styled-components';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
+// style isDraggingOver
+const Styles = styled.div``;
+
 export default function DraggableReorderList({ items, itemsActions }) {
-  const handleDragEnd = result => {
-    console.log(result);
+  const handleDragEnd = ({ source, destination }) => {
+    const { index: from } = source;
+    const to = destination ? destination.index : null;
+
+    if (to === null || from === to) return;
+
+    itemsActions.reorderItems(from, to);
   };
 
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
-      <Droppable droppableId="droppable">
+      <Droppable droppableId="itemList">
         {(provided, snapshot) => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
+          <Styles
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            isDraggingOver={snapshot.isDraggingOver}
+          >
             {items.map((item, index) => (
-              <Draggable key={item.id} draggableId={item.id} index={index}>
+              <Draggable key={item.id} draggableId={`item-${item.id}`} index={index}>
                 {(provided, snapshot) => (
                   <div ref={provided.innerRef} {...provided.draggableProps}>
                     <ListItem
@@ -22,13 +35,14 @@ export default function DraggableReorderList({ items, itemsActions }) {
                       index={index}
                       actions={itemsActions}
                       dragHandleProps={provided.dragHandleProps}
+                      isDragging={snapshot.isDragging}
                     />
                   </div>
                 )}
               </Draggable>
             ))}
             {provided.placeholder}
-          </div>
+          </Styles>
         )}
       </Droppable>
     </DragDropContext>
